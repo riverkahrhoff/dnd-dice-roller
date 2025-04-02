bf9b6ae83b04e867656e17c3e9dbb4ea423ab776
riverkahrhoff <river.kahrhoff@gmail.com>
2025-04-01 20:40:17 -0500

    removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import DiceRollerNew from "./components/DiceRollerNew";
 import NavBar from "./components/NavBar";
 import { Provider } from "./components/ui/provider";
 import { Box } from "@chakra-ui/react";
-import TextColorButton from "./components/TextColorButton";
 
 function App() {
   return (
